d5822e926376e46895767041c3961dfa299824d3
Jeff Falberg <auronzeroo@aim.com>
2020-04-21 21:00:53 -0400

    Dynamic URL querying
    Followed instructions from https://www.youtube.com/watch?v=xTumGVC90_0

--- flask_movie/app.py
+++ flask_movie/app.py
@@ -34,6 +34,12 @@ def index():
     return render_template("add_user.html", users=users, one_item=one_item)
 
 
+@app.route("/profile/<username>")
+def profile(username):
+    user = User.query.filter_by(username=username).first()
+    return render_template("profile.html", user=user)
+
+
 @app.route("/post_user", methods=["POST"])
 def post_user():
     user = User(request.form['username'], request.form['email'])
